089b128b4319a1e66675a5b90a96388d8b15fe42
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-04-30 00:30:21 -0300

    import components and variable of state and open/close  modal functions created

---
+++ Anotations.md
@@ -0,0 +1,4 @@
+**MirageJS**
+Serve para criar uma API fake, pois, pode ser que em certos momentos o Frontend termine o seu lado primeiro
+que o Backend, então é interessante para o Front já deixar conectado com alguma API, e com o Mirage pode ser feito
+uma fake.
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+import { Dashboard } from "./components/Dashboard"
+import { Header } from "./components/Header"
+import { NewTransactionModal } from "./components/NewTransactionModal"
+
+import { GlobalStyle } from "./styles/global"
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Header
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </>
   )
 }

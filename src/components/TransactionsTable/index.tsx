cbc92f2b59458fe1aa7231a043672a1e681b250a
JhonatanGAlves <jhonatangalves96@gmail.com>
2022-05-01 01:23:24 -0300

    making use of context

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,25 +1,13 @@
-import { useState, useEffect } from "react"
-import { api } from "../../services/api"
+import { useTransactions } from "../../hooks/useTransactions"
 import { Container } from "./styles"
 
-type TransactionsTableProps = {
-  id: number,
-  title: string,
-  amount: number,
-  type: string,
-  category: string,
-  createdAt: string
-}
-
 export const TransactionsTable = () => {
-  const [transactions, setTransactions] = useState<TransactionsTableProps[]>([])
-
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data))
-  }, [])
-
-  console.log({ transactions })
+  // Através do hook useContext, eu consigo ter acesso a todo o conteúdo que está sendo passado em TransactionsContext.
+  // Att: No decorrer da aula, foi criado um hook com o nome de useTransactions. Dessa forma, evita a importação
+  // do useContext e o component.
+  const {
+    transactions
+  } = useTransactions()
 
   return (
     <Container>
@@ -33,18 +21,23 @@ export const TransactionsTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>29/04/2022</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$1.100</td>
-            <td>Casa</td>
-            <td>21/03/2022</td>
-          </tr>
+          {transactions && transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App'
 
 createServer({
+  // Model seria o banco de dados do miragejs, ou seja, foi criado uma propriedade models, onde a primeira
+  // "tabela" seria transactions e ela é do tipo Model, ou seja, um banco de dados.
+  models: {
+    transaction: Model
+  },
+
+  // É umna função responsavel por pré carregar dados fakes dentro da tabela (transaction) do nosso
+  // banco de dados (schema).
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-04-29 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2022-04-18 13:23:34')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api' // Ta dizendo que todas as chamadas que fizer para API, estara a partir desse nome 'api'.
 
     // Ou seja, quando houver uma requisição do tipo get, vai buscar pelo endpoint api/transactions e retornar algo.
     // No caso, uma lista de objetos.
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      // Está pegando tudo da minha tabela transactions que está no meu banco de dados (schema).
+      return this.schema.all('transaction')
+    })
+
+    // request por padrão envia string, então como eu quero que seja enviado algo como JSON, é preciso parsear
+    // o request.requestBody para o formato JSON.
+    this.post('/transactions', (schema, request) => {
+      // Pegou os valores da requisição.
+      const data = JSON.parse(request.requestBody)
+
+      // Retornou os valores da requisição. O schema seria o banco de dados, então esta passando dados
+      // que seria o data para dentro da tabela transactions.
+      return schema.create('transaction', data)
     })
   }
 })
